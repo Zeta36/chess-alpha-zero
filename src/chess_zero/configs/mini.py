e7e48652df825566b4ffa28313226b2dadfcc6b7
Akababa <michael3795@gmail.com>
2017-12-17 12:10:57 -0600

    param tweaks..

--- src/chess_zero/configs/mini.py
+++ src/chess_zero/configs/mini.py
@@ -28,9 +28,9 @@ class PlayConfig:
         self.noise_eps = 0.25
         self.dirichlet_alpha = 0.3
         self.change_tau_turn = 10
-        self.virtual_loss = 3
+        self.virtual_loss = 2
         self.parallel_search_num = 16
-        self.prediction_worker_sleep_sec = 0.01
+        self.prediction_worker_sleep_sec = 0.001
         self.resign_threshold = -0.8 # don't resign
         self.min_resign_turn = 5
         self.average_chess_movements = 50
